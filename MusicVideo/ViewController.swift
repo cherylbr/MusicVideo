9c45cec0323fb528af1761eeddd84f4e74ef4e65
Cheryl Broder <cherylbroder@cherylbodersair.attlocal.net>
2016-07-26 15:05:29 -0500

    Initial Commit

---
+++ MusicVideo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicVideo
+//
+//  Created by Cheryl Broder on 26/07/2016.
+//  Copyright © 2016 Cheryl Broder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
