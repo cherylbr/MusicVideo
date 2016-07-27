00959f9c825304f74cdabda750e2d4d4d1cdbb34
Cheryl Broder <cherylbroder@cherylbodersair.attlocal.net>
2016-07-27 09:21:17 -0500

    Committed Part 2 coding session

--- MusicVideo/ViewController.swift
+++ MusicVideo/ViewController.swift
@@ -12,14 +12,32 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    
+        // call API with callback function (completion handler)
+        let api = APIManager()
+        api.loadData("https://itunes.apple.com/us/rss/topmusicvideos/limit=10/json",completion: didLoadData)
     }
+    
+//        // call API with trailing closure
+//        let api = APIManager()
+//        api.loadData("https://itunes.apple.com/us/rss/topmusicvideos/limit=10/json") {
+//                (result:String) in
+//                print(result)
+//            }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+//    )
+    
+    func didLoadData(result: String) {
+        
+        let alert = UIAlertController(title: (result), message: nil, preferredStyle: .Alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .Default) {
+            action -> Void in
+        }
+        alert.addAction(okAction)
+        self.presentViewController(alert, animated: true, completion: nil)
+        
+        print(result)
     }
-
-
 }
-
