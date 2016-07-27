1cd6eb7854b54f8b1a5d9eb883a38b9f3b7b635c
Cheryl Broder <cherylbroder@cherylbodersair.attlocal.net>
2016-07-27 16:31:32 -0500

    added new fields from api to our app

--- MusicVideo/MusicVideo.swift
+++ MusicVideo/MusicVideo.swift
@@ -16,6 +16,17 @@ class Videos {
     private var _vImageURL:String
     private var _vVideoURL:String
     
+    private var _vRights: String
+    private var _vPrice: String
+    private var _vArtist:String
+    private var _vImid:String
+    private var _vGenre:String
+    private var _vLinkToiTunes:String
+    private var _vReleaseDte:String
+    
+    // this variable gets created from the UI
+    var vImageData: NSData?
+    
     // Make a getter
     
     var vName: String {
@@ -29,7 +40,42 @@ class Videos {
     var vVideoURL: String {
         return _vVideoURL
     }
+    
+    var vRights: String {
+        return _vRights
+    }
+        
+    var vPrice: String {
+        return _vPrice
+    }
+        
+    var vArtist: String {
+        return _vArtist
+    }
+                    
+    var vImid: String {
+        return _vImid
+    }
+                    
+    var vGenre: String {
+        return _vGenre
+    }
+        
+    var vLinkToiTunes: String {
+        return _vLinkToiTunes
+    }
+                                
+    var vReleaseDte: String {
+        return _vReleaseDte
+    }
+                                
+
+    
+        
+        
+    
     init (data: JSONDictionary) {
+ 
         
         if let name = data["im:name"] as? JSONDictionary,
             vName = name["label"] as? String {
@@ -38,7 +84,8 @@ class Videos {
         else {
             _vName = ""
         }
-    
+ 
+        
     
         if let img = data["im:image"] as? JSONArray,
             image = img[2] as? JSONDictionary,
@@ -49,7 +96,9 @@ class Videos {
         else {
          _vImageURL = ""
         }
-    
+
+        
+        
         if let video = data["link"] as? JSONArray,
             vURL = video[1] as? JSONDictionary,
             vHref = vURL["attributes"] as? JSONDictionary,
@@ -59,6 +108,74 @@ class Videos {
         else {
             self._vVideoURL = ""
         }
-    
-}
+ 
+        
+        
+        if let rights = data["rights"] as? JSONDictionary,
+            vRights = rights["label"] as? String {
+                self._vRights = vRights
+        }
+        else {
+            _vRights = ""
+        }
+        
+        
+        if let price = data["im:price"] as? JSONDictionary,
+            vPrice = price["label"] as? String {
+                self._vPrice = vPrice
+        }
+        else {
+            _vPrice = ""
+        }
+
+        
+        if let artist = data["im:artist"] as? JSONDictionary,
+            vArtist = artist["label"] as? String {
+                self._vArtist = vArtist
+        }
+        else {
+            _vArtist = ""
+        }
+
+
+        
+        if let imid = data["id"] as? JSONDictionary,
+            vid = imid["attributes"] as? JSONDictionary,
+            vImid = vid["im:id"] as? String {
+                self._vImid = vImid
+        }
+        else {
+            _vImid = ""
+        }
+
+        
+        if let genre = data["category"] as? JSONDictionary,
+            rel2 = genre["attributes"] as? JSONDictionary,
+            vGenre = rel2["term"] as? String {
+                self._vGenre = vGenre
+        }
+        else {
+            _vGenre = ""
+        }
+        
+
+        if let release2 = data["id"] as? JSONDictionary,
+            vLinkToiTunes = release2["label"] as? String {
+                self._vLinkToiTunes = vLinkToiTunes
+        }
+        else {
+            _vLinkToiTunes = ""
+        }
+        
+      
+        if let release2 = data["im:releaseDate"] as? JSONDictionary,
+            rel2 = release2["attributes"] as? JSONDictionary,
+            vReleaseDte = rel2["label"] as? String {
+                self._vReleaseDte = vReleaseDte
+        }
+        else {
+            _vReleaseDte = ""
+        }
+
+    }
 }
