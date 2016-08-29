6901f2e8022021dd78f35a0b376873de47df9874
Cheryl Broder <cherylbroder@cherylbodersair.attlocal.net>
2016-08-29 13:48:00 -0500

    Completed Part 11

--- MusicVideo/ViewController.swift
+++ MusicVideo/ViewController.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     
     var videos = [Videos]()
+    
+    @IBOutlet weak var tableView: UITableView!
     
     @IBOutlet weak var displayLabel: UILabel!
     
@@ -24,7 +26,7 @@ class ViewController: UIViewController {
         
         // call API with callback function (completion handler)
         let api = APIManager()
-        api.loadData("https://itunes.apple.com/us/rss/topmusicvideos/limit=10/json",completion: didLoadData)
+        api.loadData("https://itunes.apple.com/us/rss/topmusicvideos/limit=50/json",completion: didLoadData)
     }
     
 //        // call API with trailing closure
@@ -51,6 +53,8 @@ class ViewController: UIViewController {
         for (index, item) in videos.enumerate() {
             print("\(index) name = \(item.vName)")
         }
+        
+        tableView.reloadData()
     }
     
     func reachabilityStatusChanged() {
@@ -66,6 +70,27 @@ class ViewController: UIViewController {
     }
     deinit {
         NSNotificationCenter.defaultCenter().removeObserver(self, name: "ReachStatusChanged", object: nil)
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return videos.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+        
+        let video = videos[indexPath.row]
+    
+        cell.textLabel?.text = ("\(indexPath.row + 1)")
+        
+        cell.detailTextLabel?.text = video.vName
+        
+        return cell
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+       return 1
     }
     
 }
