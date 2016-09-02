1f1add96929f47db286e680238d8a1e9eafaf439
Cheryl <cheryl@anitas-mbp.attlocal.net>
2016-09-02 14:39:30 -0500

    completed part 24

--- MusicVideo/SettingsTVC.swift
+++ MusicVideo/SettingsTVC.swift
@@ -47,8 +47,22 @@ class SettingsTVC: UITableViewController {
         
         touchID.on = NSUserDefaults.standardUserDefaults().boolForKey("SecSetting")
         
+        
+        if NSUserDefaults.standardUserDefaults().objectForKey("APICNT") != nil {
+            
+            let theValue = NSUserDefaults.standardUserDefaults().objectForKey("APICNT") as! Int
+            APICnt.text = "\(theValue)"
+            sliderCnt.value = Float(theValue)
+            
+        }
     }
  
+    @IBAction func valueChanged(sender: AnyObject) {
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setObject(Int(sliderCnt.value), forKey: "APICNT")
+        APICnt.text = ("\(Int(sliderCnt.value))")
+    }
     
     
     @IBAction func touchidSec(sender: UISwitch) {
